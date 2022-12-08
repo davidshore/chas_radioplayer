1e7b8cd8c6811b4c4973ae9290021bd155912fb1
iida hellström <115156204+chasacademy-iida-hellstrom@users.noreply.github.com>
2022-12-08 21:43:14 +0100

    uppgift 1

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -6,3 +6,39 @@
 // <audio controls>
 //   <source src="" type="audio/mpeg" />
 // </audio>
+ 
+
+//const url = "https://api.sr.se/api/v2/channels/?format=json";
+const url = "https://api.sr.se/api/v2/channels/?format=json&size=4"
+async function getSometing() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  const channels = document.querySelector(".radioList");
+
+  data.channels.forEach((radioList) => {
+    const div = document.createElement("div");
+    div.innerHTML = `<div><img src="${radioList.image}"</div>
+    <div style="background-color: #${radioList.color}">
+    <p>"${radioList.tagline}"</p>
+    <audio controls 
+       <source src="${radioList.liveaudio.url}" type="audio/mpeg"  />
+       
+    </audio>`;
+
+    channels.appendChild(div);
+    div.setAttribute("class", "radioList");
+  });
+}
+getSometing();
+
+
+
+  
+
+
+
+
+
+
