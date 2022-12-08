c590136c1280f693763d3794c0cc5c0e935c0573
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2022-12-08 20:24:50 +0100

    Update radioplayer.js

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -1,3 +1,5 @@
+
+
 // Steg 1. Gör en fetch till 'https://api.sr.se/api/v2/channels/?format=json'
 
 // Steg 2. loopa med tex forEach över data.channels - ta ut data och visa på html-sidan.
@@ -6,3 +8,12 @@
 // <audio controls>
 //   <source src="" type="audio/mpeg" />
 // </audio>
+const channelUrl = 'https://api.sr.se/api/v2/channels/?format=json';
+const p1 = document.querySelector('.p1')
+const click = document.querySelector('.click');
+click.addEventListener('click',()=>{
+    fetch(channelUrl)
+    .then(response=> response.json())
+    .then(radioData=>console.log(radioData.channels[1].id))
+    p1.textContent +=` ${ radioData.channels[1].id }`
+})
