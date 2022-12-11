a1a1f507f5bfd8075f936568597b5dfc84c3790b
nawrazaltai <nawraz.altai@chasacademy.se>
2022-12-11 21:22:19 +0100

    task completed

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -1,8 +1,80 @@
-// Steg 1. Gör en fetch till 'https://api.sr.se/api/v2/channels/?format=json'
+// Select loader
+let loader = document.querySelector("#loader");
 
-// Steg 2. loopa med tex forEach över data.channels - ta ut data och visa på html-sidan.
+// Select inputfield
+const inputField = document.querySelector("#inputField");
 
-// Steg 3. ta ut liveaudio.url från varje kanal och lägg i en audio tagg.
-// <audio controls>
-//   <source src="" type="audio/mpeg" />
-// </audio>
+// Add eventlistener to the inputfield
+inputField.addEventListener("input", () => {
+
+// Store inputvalue in variable
+let inputValue = inputField.value.toUpperCase();
+
+// Show loader when user enters characters in inputfield
+loader.style.display="inline-block";
+
+// Fetch Radio Stations
+async function getData() {
+    const response = await fetch("https://api.sr.se/api/v2/channels/?format=json");
+    const data = await response.json();
+    
+    // Hide loader when fetch is done
+    loader.style.display="none";
+
+        // Filter channels with inputvalue
+        let filteredChannels = data.channels.filter((i) => {
+            return i.name.startsWith(inputValue);
+        });
+
+        // Clear the channelContainer with each fetch
+        const channelContainer = document.querySelector("#channels");
+        channelContainer.innerHTML = " ";
+          
+        filteredChannels.forEach((channel) => {
+          
+          // Create div for each channel
+          const channelElement = document.createElement("div");
+          channelElement.setAttribute("class", "channelElement");
+          
+          // Create left div for the channel pic & set its classname and content:
+          const channelPicDiv = document.createElement("div");
+          channelPicDiv.setAttribute("class", "picDiv");
+          channelPicDiv.innerHTML = `<img src="${channel.image}" alt="Radio channel picture">`;
+          channelElement.appendChild(channelPicDiv);
+
+          // Create right div to hold text and audio tag;
+          const channelRightDiv = document.createElement("div");
+          channelRightDiv.setAttribute("class", "rightDiv");
+          channelRightDiv.style.backgroundColor = `#${channel.color}`;
+          
+          // Create right upper div to hold channel name + tag
+          const upperRightDiv = document.createElement("div");
+          upperRightDiv.setAttribute("class", "upperRightDiv");
+          upperRightDiv.innerHTML = `
+          <h1>${channel.name}</h1>
+          <p>${channel.tagline}</p>`;
+          
+          // Create right lower div to hold channel audio tag
+          const lowerRightDiv = document.createElement("div");
+          lowerRightDiv.setAttribute("class", "lowerRightDiv");
+          lowerRightDiv.innerHTML = `
+          <audio controls><source src="${channel.liveaudio.url}" type="audio/mpeg"/></audio>`;
+
+          // append the upper & lower right divs to the main right div
+          channelRightDiv.appendChild(upperRightDiv);
+          channelRightDiv.appendChild(lowerRightDiv);
+
+          // append main right div to the channel element
+          channelElement.appendChild(channelRightDiv);
+
+          // append each channel div to the container div
+          channelContainer.appendChild(channelElement);
+
+          // Hide all channels if inputfield is empty
+          if (inputValue.length <= 0) {
+            channelContainer.innerHTML = "";
+          }
+  })}
+    getData();
+  });
+
