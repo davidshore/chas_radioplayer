bafde07ba5c7cb965a0dae8b0f24f44164e2e91e
Camilla Sundbom <Camilla.Sundbom@outlook.com>
2022-12-12 09:10:49 +0100

    finished the tasks

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -6,3 +6,27 @@
 // <audio controls>
 //   <source src="" type="audio/mpeg" />
 // </audio>
+
+const url = "https://api.sr.se/api/v2/channels/?format=json&size=4";
+async function getRadio() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.table(data.channels);
+
+  const channelsDiv = document.querySelector(".allChannels");
+
+  data.channels.forEach((channel) => {
+    const div = document.createElement("div");
+    div.innerHTML = `<img src="${channel.image}" />
+    <div style="background-color: #${channel.color}">
+    <p>"${channel.tagline}"</p>
+    <audio controls>
+       <source src="${channel.liveaudio.url}" type="audio/mpeg"  />
+    </audio>
+    </div>`;
+
+    channelsDiv.appendChild(div);
+    div.setAttribute("class", "allChannelsChild");
+  });
+}
+getRadio();
