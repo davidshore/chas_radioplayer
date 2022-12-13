71b89477b615dd61ac4aed804d074c7fe01781a6
Furkan Abay <furkan_abay@hotmail.com>
2022-12-13 17:14:56 +0100

    Furkan Abay

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -1,4 +1,29 @@
+const container = document.querySelector(".container");
+
 // Steg 1. Gör en fetch till 'https://api.sr.se/api/v2/channels/?format=json'
+async function radioDataFetch() {
+	const url = "http://api.sr.se/api/v2/channels?format=json&size=100";
+	const response = await fetch(url)
+		.then((response) => (response = response.json()))
+		.then((response) => (data = response));
+	data = data.channels;
+
+	console.log(data);
+
+	data.forEach((channel) => {
+		container.innerHTML += `<div class="radio" style="background-color: #${channel.color}">
+		<img class="radio-img" src=${channel.image}>
+		<section class="right-side">
+			<h2 class="radio-name">${channel.name}</h2>
+			<audio controls>
+				<source src=${channel.liveaudio.url} type="audio/mpeg" >
+			</audio>
+		</section>
+	</div>`;
+	});
+}
+
+radioDataFetch();
 
 // Steg 2. loopa med tex forEach över data.channels - ta ut data och visa på html-sidan.
 
