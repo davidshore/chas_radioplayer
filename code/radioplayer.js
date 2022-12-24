8639b543f76b32e4261e86b2e6ff913391e20132
ChasAcademy-Fernando <115954598+ChasAcademy-Fernando@users.noreply.github.com>
2022-12-24 03:19:50 +0100

    RadioPLayer_Fernando

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -6,3 +6,36 @@
 // <audio controls>
 //   <source src="" type="audio/mpeg" />
 // </audio>
+
+
+async function getApi(){
+    const response = await fetch("https://api.sr.se/api/v2/channels/?format=json")
+    const data = await response.json();
+    const channelsContainer = document.querySelector("#channels");
+        
+    data.channels.forEach(element => {
+        const channelDiv = document.createElement('div')
+        channelDiv.setAttribute('class','channels')
+        channelsContainer.appendChild(channelDiv)
+        channelDiv.style.backgroundColor = "#" + element.color;
+        channelDiv.innerHTML = `
+            <div class="leftSide" >
+             <img src="${element.image}"
+            </div>
+            <div class="rightSide" >
+                <h1>${element.name}</h1>
+                <audio controls>
+                <source src="${element.liveaudio.url}" type="audio/mpeg">
+                Your browser does not support the audio element.
+                </audio>
+            </div>`;
+    
+    
+    });
+    
+    
+    
+    
+    }
+    
+    getApi()
